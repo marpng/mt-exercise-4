2ee8fab3b57ce1892ac79a1557819d5261a914df
marpng <116029708+marpng@users.noreply.github.com>
2024-05-04 21:52:18 +0200

    Add files via upload

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,3 +1,33 @@
+##! /bin/bash
+#
+#scripts=$(dirname "$0")
+#base=$scripts/..
+#
+#models=$base/models
+#configs=$base/configs
+#
+#mkdir -p $models
+#
+#num_threads=10
+##device=0
+#
+## measure time
+#
+#SECONDS=0
+#
+#logs=$base/logs
+#
+#model_name=deen_transformer_regular
+#
+#mkdir -p $logs
+#
+#mkdir -p $logs/$model_name
+#
+#OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+#
+#echo "time taken:"
+#echo "$SECONDS seconds"
+
 #! /bin/bash
 
 scripts=$(dirname "$0")
@@ -9,7 +39,6 @@ configs=$base/configs
 mkdir -p $models
 
 num_threads=10
-#device=0
 
 # measure time
 
@@ -17,13 +46,22 @@ SECONDS=0
 
 logs=$base/logs
 
-model_name=deen_transformer_regular
-
-mkdir -p $logs
-
+# First Model
+model_name="deen_transformer_pre"
 mkdir -p $logs/$model_name
-
+echo "Config path: $configs/$model_name.yaml"
 OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
 
-echo "time taken:"
+echo "time taken for pre-norm model:"
+echo "$SECONDS seconds"
+
+# Reset timer
+SECONDS=0
+
+# Second Model
+model_name="deen_transformer_post"
+mkdir -p $logs/$model_name
+OMP_NUM_THREADS=$num_threads python -m joeynmt train $configs/$model_name.yaml > $logs/$model_name/out 2> $logs/$model_name/err
+
+echo "time taken for post-norm model:"
 echo "$SECONDS seconds"
